e38135a6a046e463be640e97aee745a62abfce26
Jakob Butler <jb213215@ohio.edu>
2020-02-03 10:36:10 -0500

    Monday Class

---
+++ DAL/Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace jb213215_MIS4200.DAL
+{
+    public class Context : DbContext
+    {
+        public Context() : base("name=DefaultConnection")
+        { 
+        
+        }
+
+        public System.Data.Entity.DbSet<jb213215_MIS4200.Models.Customer> Customers { get; set; }
+
+        public System.Data.Entity.DbSet<jb213215_MIS4200.Models.Order> Orders { get; set; }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jb213215_MIS4200.Models
+{
+    public class Order
+    {
+        public int orderId { get; set; }
+        public string description { get; set; }
+        public DateTime orderDate { get; set; }
+        
+
+        
+    }
+}
